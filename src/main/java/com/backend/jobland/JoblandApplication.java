abf7f53b5a38bfbdb0cb12976a9a49f27295cc15
jonik2909 <boronovjonibek@gmail.com>
2026-05-02 02:19:55 +0900

    feat: initial setup

---
+++ src/main/java/com/backend/jobland/JoblandApplication.java
@@ -0,0 +1,13 @@
+package com.backend.jobland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoblandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoblandApplication.class, args);
+	}
+
+}
