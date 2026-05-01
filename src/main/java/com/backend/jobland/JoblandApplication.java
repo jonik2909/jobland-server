3ee142ea7040a945f736640c32d5595dab7239c0
jonik2909 <boronovjonibek@gmail.com>
2026-05-02 04:17:32 +0900

    feat: integrate dot env

--- src/main/java/com/backend/jobland/JoblandApplication.java
+++ src/main/java/com/backend/jobland/JoblandApplication.java
@@ -5,15 +5,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class JoblandApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(JoblandApplication.class);
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(JoblandApplication.class, args);
 
-		log.info("Jobland Backend started successfully on port: " + "8080");
+		log.info("Jobland Backend started successfully on port: " + System.getProperty("PORT"));
 	}
 
 }
