c04521ec0ec6bbdabf64843e70e3a5eeba099713
jonik2909 <boronovjonibek@gmail.com>
2026-05-02 02:32:02 +0900

    feat: integrate spring boot devtools and Logger

--- src/main/java/com/backend/jobland/JoblandApplication.java
+++ src/main/java/com/backend/jobland/JoblandApplication.java
@@ -1,13 +1,19 @@
 package com.backend.jobland;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JoblandApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(JoblandApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(JoblandApplication.class, args);
+
+		log.info("Jobland Backend started successfully on port: " + "8080");
 	}
 
 }
